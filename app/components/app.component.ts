6f0f07920eb44ccc788771805a99aa76eb02887c
Vitaliy Shebela <vitalik.privat@gmail.com>
2016-12-29 10:15:21 +0200

    Initial commit

---
+++ app/components/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'weather-app',
+    templateUrl: 'app/assets/html/app.component.html',
+    styleUrls: ['app/assets/css/app.component.css']
+})
+export class AppComponent {
+}
